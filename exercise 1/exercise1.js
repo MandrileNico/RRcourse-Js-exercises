e3b1508d05ac2fe0510e8ad03c40f19d657fc726
Nicolás Mandrile <mandrilenico@gmail.com>
2020-10-25 12:40:46 -0300

    Exercise 1

---
+++ exercise 1/exercise1.js
@@ -0,0 +1,15 @@
+//1-a
+var a = 5;
+var b = 6;
+var c = a + b;
+console.log('The value of A is: '+ a, 'the value of B is: '+ b, 'and the sum is: '+ c);
+//1-b
+var word1 = 'Hello';
+var word2 = ' World';
+var word3 = word1 + word2;
+console.log('The word 1  is: '+ word1, 'the word 2 is: '+ word2, 'and the sentence is: '+ word3);
+//1-c
+var str1 = 'Nice';
+var str2 = 'day';
+var long = str1.length+str2.length;
+console.log('The long of the sentence Nice day is: '+ long); 
