466ba97e046b0264116aea96743ba72db33feaa9
Nicolás Mandrile <mandrilenico@gmail.com>
2020-10-25 15:56:45 -0300

    Second exercise

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # RRcourse-Js-exercises
 ---
+Here you will find the solution of the exercises of week 4 
+---
 ## Author
 
 Mandrile Nicolas
---
+++ exercise 2/exercise2.js
@@ -0,0 +1,23 @@
+//2-a
+var str1 = 'Hello, I am Nicolás';
+var up1 = str1.toLocaleUpperCase();
+console.log('The sentece is: '+ up1);
+//2-b
+var str2 = 'Hello, I am Nicolás';
+var substr2 = str2.substr(0, 5);
+console.log('The first five letters are:'+ substr2 );
+//2-c
+var subtr3 = str2.substr(-3)
+console.log('The last 3 letters are:'+ subtr3 );
+//2-d
+var str3 = 'hello, I am Nicolás';
+var subtr4 = str3[0].toLocaleUpperCase()+str3.substr(1).toLocaleLowerCase(); 
+console.log('the original sentence:'+ str3, 'The result:' + subtr4);
+//2-e
+var find = str3.indexOf(' ')
+console.log('The first blanc space are in the position: '+ find);
+//2-f
+var str4 = 'economia reseciva';
+var final = str4[0].toLocaleUpperCase() + str4.substr(1 , str4.indexOf(' ')).toLocaleLowerCase() + 
+            str4[str4.indexOf(' ')+1].toLocaleUpperCase() + str4.substr(str4.indexOf(' ')+2).toLowerCase();
+console.log(final)
