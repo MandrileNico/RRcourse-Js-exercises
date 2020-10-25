e7a515b8d0f48556d5ecec44a8b24bf845c88c84
Nicolás Mandrile <mandrilenico@gmail.com>
2020-10-25 18:38:50 -0300

    Exercise 5

---
+++ exercise 5/exercise5.js
@@ -0,0 +1,23 @@
+// 5-a
+var array1 = ["Me", "voy", "a", "tomar", "Vacaciones"];
+for (var i = 0; i < 5; i++){
+    alert('La palabra de la posicion ' + i + 'es: ' + array1[i]);
+}
+//5-b
+for (var i = 0; i < 5; i++){
+    var array2 = array1[i];
+    array1[i] = array2[0].toLocaleUpperCase() + array2.substr(1, array2.length-1);
+    alert(array1[i]); 
+}
+//5-c
+var sentence = [];
+for (var i = 0; i < 5; i++){
+    sentence[i] = array1[i];
+}
+alert(sentence);
+//5-d
+var array3 = []
+for (var i = 0; i < 10; i++){
+    array3[i]=i;
+}
+console.log(array3);
