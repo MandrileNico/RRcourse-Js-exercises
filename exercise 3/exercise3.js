08fad031aa57db6cfd06cae64c709d26681960b6
Nicolás Mandrile <mandrilenico@gmail.com>
2020-10-27 10:18:51 -0300

    Corrections in exercise 3

--- exercise 3/exercise3.js
+++ exercise 3/exercise3.js
@@ -1,24 +1,24 @@
 //3-a
-var array1 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+const array1 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
             "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-console.log(array1[5], array1[11]);
+console.log(array1[4], array1[10]);
 //3-b
-array1.sort();
-console.log('The alphabetically ordered array is: '+ array1);
+var array2 = array1.slice();
+array2.sort();
+console.log('The alphabetically ordered array is: '+ array2);
 //3-c
-array1.unshift("Vacaciones") + array1.push("Fiestas");
-console.log('The array is: '+ array1);
+array2.unshift("Vacaciones") + array2.push("Fiestas");
+console.log('The array is: '+ array2);
 //3-d
-array1.shift() + array1.pop(); 
-console.log('the array without the added elements: ' + array1)
+array2.shift() + array2.pop(); 
+console.log('the array without the added elements: ' + array2)
 //3-e
-array1.reverse();
-console.log('the inverted array: '+ array1);
+array2.reverse();
+console.log('the inverted array: '+ array2);
 //3-f
-array1 = array1.join('');
-console.log('The join array : ' + array1);
+array2 = array2.join('-');
+console.log('The join array : ' + array2);
 //3-g
-var array2 = array1;
-array2 = array2.slice(array2.indexOf('N'), array2.indexOf('Marzo'));
-console.log('The new array is:' + array2);
+array3 = array1.slice(array1.indexOf('Mayo'), array1.indexOf('Noviembre')+1);
+console.log('The new array is:' + array3);
 
